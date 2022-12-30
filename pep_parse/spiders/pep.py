054814d293d6055935f283c3e4e395d6796affad
Вячеслав Роев <roev96@yandex.ru>
2022-12-30 22:45:55 +0300

    review1

---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,26 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        all_peps = response.css("a.pep.reference.internal::attr('href')").getall()
+        for pep in all_peps:
+            yield response.follow(pep, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        title = response.css('h1.page-title::text').get()
+        number = int(title.split(" ")[1])
+        name = title[(title.find(" – ") + 3) :]
+        status = response.css('dt:contains("Status") + dd abbr::text').get()
+        data = {
+        'number': number,
+        'name': name,
+        'status': status
+        }
+        yield PepParseItem(data)
