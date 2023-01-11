646ae796f5119548028699fae3d4a998e824efaa
Вячеслав Роев <roev96@yandex.ru>
2023-01-11 22:37:17 +0300

    fix const

--- pep_parse/constaints.py
+++ pep_parse/constaints.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DOMAIN = os.getenv('DOMAIN')
-URL = os.getenv('URL')
+DOMAIN = os.getenv('DOMAIN', default='peps.python.org')
+URL = os.getenv('URL', default='https://peps.python.org/')
